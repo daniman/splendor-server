93e5b8189f8050c12e8b004c28bf0e232faeee3e
Danielle Man <daniman0808@gmail.com>
2020-04-28 20:01:40 -0700

    use env var for port

--- index.js
+++ index.js
@@ -120,6 +120,10 @@ const server = new ApolloServer({
   },
 });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+server
+  .listen({
+    port: process.env.PORT || 4000,
+  })
+  .then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
