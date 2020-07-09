5cce7c80cd1bbc20a3cee6d0fff1ba1dcfd35c0c
Stephen Barlow <stephenbarlow@apollo-stephenbarlow.gateway.sonic.net>
2020-07-09 14:00:25 -0700

    Show secret reserve cards when game is complete

--- index.js
+++ index.js
@@ -87,6 +87,7 @@ const resolvers = {
       return game.players.map((p) => ({
         ...p,
         reservedCards: p.reservedCards.map((rc) =>
+          game.state === 'COMPLETE' ||
           !rc.isPrivate ||
           p.id === args.currentPlayer ||
           args.currentPlayer === 'sudo'
