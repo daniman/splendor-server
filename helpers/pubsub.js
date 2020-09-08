c6350f67e9512641f3d7d49cfe887851e3ca1c67
Danielle Man <man.danielleh@gmail.com>
2020-09-07 22:56:13 -0700

    add redis config to websockets/pubsubs

--- helpers/pubsub.js
+++ helpers/pubsub.js
@@ -1,3 +1,13 @@
+const Redis = require("ioredis");
 const { RedisPubSub } = require("graphql-redis-subscriptions");
-const pubsub = new RedisPubSub();
+
+const pubsub = new RedisPubSub({
+  publisher: process.env.REDIS_URL
+    ? new Redis(process.env.REDIS_URL)
+    : new Redis(),
+  subscriber: process.env.REDIS_URL
+    ? new Redis(process.env.REDIS_URL)
+    : new Redis(),
+});
+
 module.exports = pubsub;
